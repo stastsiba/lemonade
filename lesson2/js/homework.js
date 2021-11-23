c620200275145c413a05f4685248fd6fa9b83031
stastsiba <88132424+stastsiba@users.noreply.github.com>
2021-11-23 15:48:28 +0300

    Update homework.js

--- lesson2/js/homework.js
+++ lesson2/js/homework.js
@@ -11,4 +11,40 @@ let firstPlace,
 // если место зарезервировано(тоесть каждое третее) выводим алерт с текстом 'место забронированно'
 // решить задание 2-мя способами: if, switch...case
 
+firstPlace = prompt ('Укажите место которое желаете забронировать?');
+    secondPlace = +firstPlace + 1;
 
+
+    // if (firstPlace >= 20 && firstPlace <= 30){
+    //     if(firstPlace % 3 === 0) {
+    //         secondPlace = +firstPlace + 1
+    //         alert('Место забронировано,свободно следующее место ' + secondPlace);
+    //     }else if(firstPlace > MAX_PLACES){
+    //         alert('Таких мест нет');
+    //     }else {
+    //         alert('Ваше место № ' + firstPlace);
+    //     }
+    // }else if(firstPlace == ''){
+    //     alert('Вы не указали место')
+    // }else if (firstPlace == null){
+    //     alert('Вы отменили!')
+    // }else {
+    //      alert('WARNING!!!')
+    // }
+
+   switch(true){
+        case firstPlace == '':
+           alert('Вы не выбрали место');
+           break;
+        case firstPlace == null:
+           alert('Вы отменили выбор!');
+           break;
+        case firstPlace < 20 || firstPlace > 50:
+            alert('Предупреждение!!!');
+            break;
+        case firstPlace % 3 ===0:
+            alert('Это место занято! Ваше место ' + secondPlace);
+            break;
+        default:
+            alert('Ваше место ' + firstPlace);
+   }
